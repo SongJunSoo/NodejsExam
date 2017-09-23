6a1c6dcd4b0f002319bd53b785f4bbff8f4661f1
SongJunSoo <jssong80@naver.com>
2017-09-23 15:35:19 +0900

    http 통신 예제 추가

---
+++ httpExam/json.formatter.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var output = [];
+for(var i=0; i < 100; i++){
+	output.push({
+		key: 'key_' + i,
+		value : Math.random()
+	});
+}
+
+http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'applicaton/json'});
+	response.end(JSON.stringify(output));
+}).listen(52273, function(){
+	console.log('server running at http://127.0.0.1:52273')
+})
