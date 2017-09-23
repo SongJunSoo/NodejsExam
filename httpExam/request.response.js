29e3a8feb6760e0cba604c68c816752a52ef1e50
SongJunSoo <jssong80@naver.com>
2017-09-23 16:13:47 +0900

    ejs 예제 추가

--- httpExam/request.response.js
+++ httpExam/request.response.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
 
 http.createServer(function(req,res){
     if(req.method == 'GET'){
     	console.log(req.url+"GET");
-    	fs.readFile('index.html', function(err,data){
+    	if(req.url == '/'){
+    		fs.readFile('index.html', function(err,data){
     		if(!err){
     			res.writeHead(200, {'Content-Type':'text/html'});
     			res.end(data);
@@ -12,7 +14,17 @@ http.createServer(function(req,res){
     			res.writeHead(404);
     			res.end();
     		}
-    	});
+    	});	
+    	}else if(req.url == '/ejs'){
+    		fs.readFile('template.ejs', 'utf8', function(err,data){
+    			if(!err){
+    				var html = ejs.render(data, {name:'HONG', description:'Hello World For EJS'});
+    				res.writeHead(200, {'Content-Type':'text/html'});
+    				res.end(html);
+    			}
+    		});
+    	}
+    	
     }else if(req.method == 'POST'){
     	console.log(req.url+" POST");
     	req.on('data', function(data){
