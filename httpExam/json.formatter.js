e2dc8d6507da1eafb7221483e4935ad2ed769a0e
SongJunSoo <jssong80@naver.com>
2017-09-23 16:34:05 +0900

    jade 사용 예제 추가

--- httpExam/json.formatter.js
+++ httpExam/json.formatter.js
@@ -9,7 +9,7 @@ for(var i=0; i < 100; i++){
 }
 
 http.createServer(function(request, response){
-	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.writeHead(200, {'Content-Type': 'text/json'});
 	response.end(JSON.stringify(output));
 }).listen(52273, function(){
 	console.log('server running at http://127.0.0.1:52273')
